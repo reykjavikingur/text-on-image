b73cf8d5ea9f5ea6e3f0d128c0e669890df82cea
Andrew Strader <astrader@resourceammirati.com>
2018-05-31 13:20:54 -0400

    initialize

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+var pkg = require('../../package.json');
+
+module.exports = {
+    siteName: 'Text on Image CSS Techniques',
+    appDescription: pkg.description || 'Generic description.',
+    appName: pkg.name,
+    appVersion: pkg.version,
+    appAuthor: pkg.author,
+};
